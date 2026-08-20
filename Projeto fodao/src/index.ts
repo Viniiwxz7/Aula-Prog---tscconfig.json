d181ae500f4ca9194fd557462df6bb7c7f9b9504
Vinícius Valentim Dos santos <vvs8@discente.ifpe.edu.br>
2026-08-20 11:28:23 -0300

    Add files via upload

--- Projeto fodao/src/index.ts
+++ Projeto fodao/src/index.ts
@@ -1,4 +1,24 @@
-import pessoa from "./pessoa";
+import aluno from './aluno'
+
+
+let aluno1 =  new aluno("tds-0617", "iscolar di jadi", "jader de albuquerque", 16, "9864328")
+
+let matricula = aluno1.getMatricula()
+console.log(matricula)
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import pessoa from "./pessoa";
 
 let p1 = new pessoa(" ",17," ")
 
@@ -11,4 +31,4 @@ p1.setcpf("12345678987")
 let n = p1.getnome()
 let i = p1.getidade()
 let c = p1.getcpf()
-console.log(n,i,c)
+console.log(n,i,c)*/
